f16000132da880cf8c6c8d9a657663698b9bfc69
amaankazi <43357624+amaankazi@users.noreply.github.com>
2018-11-26 17:34:32 -0500

    Add files via upload

--- motorTest.ino
+++ motorTest.ino
@@ -1,20 +1,21 @@
+int PWMOutputPin1 = 5; //only some pins on the Arduino can be used for PWM
+int PWMOutputPin2 = 11;
 
-int PWMOutputPin = 5; //only some pins on the Arduino can be used for PWM
-int DigitalOutputPin1 = 10;
-int DigitalOutputPin2 = 11;
-
+//AO-A5 only work
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(PWMOutputPin, OUTPUT);
-  pinMode(DigitalOutputPin1, OUTPUT);
-  pinMode(DigitalOutputPin2, OUTPUT);
+  pinMode(PWMOutputPin1, OUTPUT);
+  pinMode(PWMOutputPin2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  analogWrite(PWMOutputPin, 50);
+  analogWrite(PWMOutputPin1, 50);
   digitalWrite(A2, LOW);
   digitalWrite(A3, HIGH);
+  analogWrite(PWMOutputPin2, 50);
+  digitalWrite(A4, LOW);
+  digitalWrite(A5, HIGH);
   delay(1000);  
 }
