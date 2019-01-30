785ee00976f016e906f8e46322d9e7ac58caf1a7
nateriek <32182835+nateriek@users.noreply.github.com>
2019-01-29 20:03:33 -0500

    Add files via upload
    Added calibration to IR sensors

--- IRTest_oddSensors.ino
+++ IRTest_oddSensors.ino
@@ -19,6 +19,18 @@ int pitch = 4; //distance between IR sensors in mm
 int sensor_dist = 0;
 float error = 0;
 
+// calibration ranges
+int low1;
+int high1;
+int low3;
+int high3;
+int low5;
+int high5;
+int low7;
+int high7;
+int low9;
+int high9;
+
 //calculates distance between a given sensor and the middle of the array board
 //only for IR arrays with an odd number of sensors
 int sensor_distance(int sensorNum) {
@@ -45,9 +57,62 @@ void setup() {
   pinMode(AnalogInputPin2, INPUT);
   pinMode(AnalogInputPin3, INPUT);
   pinMode(AnalogInputPin4, INPUT);
-
+  calibration();
 }
 
+void calibration(){
+  int start = millis();
+  int current = millis();
+  int value1 = analogRead(AnalogInputPin0);
+  int value3 = analogRead(AnalogInputPin1);
+  int value5 = analogRead(AnalogInputPin2);
+  int value7 = analogRead(AnalogInputPin3);
+  int value9 = analogRead(AnalogInputPin4);
+  low1 = value1;high1 = value1;
+  low3 = value3;high1 = value3;
+  low5 = value5;high1 = value5;
+  low7 = value7;high1 = value7;
+  low9 = value9;high1 = value9;
+  
+  while((current-start)<5000){
+    value1 = analogRead(AnalogInputPin0);
+    value3 = analogRead(AnalogInputPin1);
+    value5 = analogRead(AnalogInputPin2);
+    value7 = analogRead(AnalogInputPin3);
+    value9 = analogRead(AnalogInputPin4);
+    if(value1 > high1){
+      high1 = value1;
+    }
+    else if(value1 < low1){
+      low1 = value1;
+    }
+    if(value3 > high3){
+      high3 = value3;
+    }
+    else if(value3 < low3){
+      low3 = value3;
+    }
+    if(value5 > high5){
+      high5 = value5;
+    }
+    else if(value5 < low5){
+      low5 = value5;
+    }
+    if(value7 > high7){
+      high7 = value7;
+    }
+    else if(value7 < low7){
+      low7 = value7;
+    }
+    if(value9 > high9){
+      high9 = value9;
+    }
+    else if(value9 < low9){
+      low9 = value9;
+    }
+    current = millis();
+  }
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -57,9 +122,18 @@ void loop() {
   ir7 = analogRead(AnalogInputPin3);
   ir9 = analogRead(AnalogInputPin4);
 
-  calculateError();
+  ir1 = map(ir1,low1,high1,0,100);
+  ir1 = constrain(ir1,0,100);
+  ir3 = map(ir3,low3,high3,0,100);
+  ir3 = constrain(ir3,0,100);
+  ir5 = map(ir5,low5,high5,0,100);
+  ir5 = constrain(ir5,0,100);
+  ir7 = map(ir7,low7,high7,0,100);
+  ir7 = constrain(ir7,0,100);
+  ir9 = map(ir9,low9,high9,0,100);
+  ir9 = constrain(ir9,0,100);
 
-  Serial.println(ir1);  
+  Serial.println(ir9);
 
   delay(100);
 
