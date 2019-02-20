392e2beaee01885bd319427fab8158ed7c776d04
nateriek <nateriek98@gmail.com>
2019-02-19 19:23:17 -0500

    Added config.h file

---
+++ config.h
@@ -0,0 +1,26 @@
+#ifndef LINEFOLLOWER_CONF_H_
+#define LINEFOLLOWER_CONF_H_
+
+#include <Arduino.h>
+
+//IR pins
+#define IR1_PIN A0
+#define IR2_PIN A1
+#define IR3_PIN A2
+#define IR4_PIN A3
+#define IR5_PIN A4
+#define IR6_PIN A5
+#define IR7_PIN A6
+#define IR8_PIN A7
+#define IR9_PIN A8
+
+#define PITCH 4 //distance between IR sensors in mm
+
+//Motor pins
+#define PWM_PIN_1 5
+#define PWM_PIN_2 11
+#define MOTOR_L_1 A9
+#define MOTOR_L_2 A10
+#define MOTOR_R_1 A11
+#define MOTOR_R_2 A12
+#define MAX_SPEED 255
