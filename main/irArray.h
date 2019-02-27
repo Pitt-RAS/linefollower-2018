97b2beb6f9c60cb1ed8d01e080b77c3693482130
Your Name <you@example.com>
2019-02-26 19:50:14 -0500

    irArray header file modified

--- main/irArray.h
+++ main/irArray.h
@@ -1,14 +1,14 @@
 class irArray {
     private:
         int pitch_;
-        char pins_[];
+        int pins_[];
         int numSensors;
 
 
     public:
         
         //constructor and functions
-        irArray::irArray(char pins[], int pitch);
+        irArray::irArray(int pins[], int pitch);
         int irArray::sensorDistance(int sensorNum);
         float irArray::interpolate(float irVal[]);
         float irArray::read();
