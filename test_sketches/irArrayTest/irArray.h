b8535e608795f2791b8dae9d39985d3ad36dc609
Cameron Brown <cambrown62@yahoo.com>
2019-03-19 19:51:16 -0400

    debugging irArraytest

--- test_sketches/irArrayTest/irArray.h
+++ test_sketches/irArrayTest/irArray.h
@@ -1,8 +1,13 @@
+#ifndef IR_ARRAY_H_
+#define IR_ARRAY_H_
+#include "config.h"
+
 class irArray {
     private:
         int pitch_;
         int pins_[];
         int numSensors;
+        float calib_from_vals[NUM_SENSORS*2];
 
 
     public:
@@ -11,8 +16,10 @@ class irArray {
         irArray::irArray(int pins[], int pitch);
         int irArray::sensorDistance(int sensorNum);
         float irArray::interpolate(float irVal[]);
-        float irArray::read();
-        void irArray::calibrate(float &calibratedVals);
-
+        float* irArray::read();
+        void irArray::calibrate();
+        float* irArray::get_calib();
 
 };
+
+#endif
--- test_sketches/irArrayTest/irArrayTest.ino
+++ test_sketches/irArrayTest/irArrayTest.ino
@@ -1,14 +1,57 @@
 #include "config.h"
-int pins[] = {IR1_PIN,IR3_PIN,IR5_PIN,IR7_PIN,IR9_PIN};
-irArray test = new irArray(pins,PITCH);
+#include "irArray.h"
 
-
+//float calib_from_vals[NUM_SENSORS*2];
+/*
+int* pins = NULL;
+pins = new int[NUM_SENSORS];
+pins[0] = IR1_PIN;
+pins[1] = IR2_PIN;
+pins[2] = IR3_PIN;
+pins[3] = IR4_PIN;
+pins[4] = IR5_PIN;
+irArray test(pins, PITCH);
+*/
 
 
 void setup() {
   Serial.begin(9600);
-  Serial.print(PITCH);
+
+  int* pins = NULL;
+  pins = new int[NUM_SENSORS];
+  pins[0] = IR1_PIN;
+  pins[1] = IR2_PIN;
+  pins[2] = IR3_PIN;
+  pins[3] = IR4_PIN;
+  pins[4] = IR5_PIN;
+  for (int i = 0; i < NUM_SENSORS; i++){
+    pinMode(pins[i], INPUT);
+  }
+  irArray test(pins, PITCH);
+  
+  
+  test.calibrate();
+  delete[] pins;
+
+  /*
+  for (int i = 0; i < 10; i++){
+    Serial.print(test.get_calib()[i]);
+    Serial.print("\n");
+  }
+  */
+
+  //test.read();
+
+/*  
+  for (int i = 0; i < 5; i++){
+    
+    //Serial.print(test.read()[i]);
+    Serial.print("\n");
+  }
+  */
 }
+
+
 void loop() {
   
 
