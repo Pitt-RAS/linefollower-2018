0083818c97f3ee11b89780925a7cfdf8c3226a85
nateriek <nateriek98@gmail.com>
2019-04-09 19:21:00 -0400

    Updated irArray CPP files

--- main/irArray.cpp
+++ main/irArray.cpp
@@ -1,76 +1,87 @@
-#include <irArray.h>
+#include "irArray.h"
 #include <Arduino.h>
-#include <config.h>
+#include "config.h"
 
-class irArray {
+
+irArray::irArray(int pins[], int pitch) {
     
-    int pitch_;
-    int pins_[];
-    int numSensors;
+    //numSensors = sizeof(pins)/sizeof(char);
+    numSensors = NUM_SENSORS;
+    //Serial.print(NUM_SENSORS);
+    pitch_ = pitch;
 
-public:
-
-    irArray::irArray(int pins[], int pitch) {
+    for(int i = 0; i < numSensors; i++) {
         
-        //numSensors = sizeof(pins)/sizeof(char);
-        numSensors = NUM_SENSORS;
+        pins_[i] = pins[i];
+        //Serial.print(pins_[i]);
+        //Serial.print("\n");
+    }
+}
 
-        pitch_ = pitch;
+float irArray::interpolate(float irVal[]) {
+    
+    //interpolation code
+    double num = 0;
+    double den = 0;
 
-        for(int i = 0; i <= numSensors; i++) {
-            pins_[i] = pins[i];
-        }
+    for(int i = 0; i < numSensors; i++) {
+        den += irVal[i];
+    }
+    den -= irVal[numSensors/2]; //exclude middle sensor
+
+    for(int i = 0; i < int(numSensors/2); i++){
+      num += (int(numSensors/2)-i)*PITCH*(irVal[numSensors-i-1]-irVal[i]);
     }
 
-    float irArray::interpolate(float irVal[]) {
-        //interpolation code
-        double num = 0;
-        double den = 0;
+   return num/den;
+}
 
-        for(int i = 0; i < int(numSensors/2); i++) {
-            num += (pitch*((numSensors/2)-i)*(irVal[numSensors-i]-irVal[i]);
-            den += irVal[i];
-        }
+float* irArray::read() {
+    float irVal[numSensors];
+    //Serial.print(numSensors);
+    for(int i = 0; i < numSensors; i++) {
+        irVal[i] = analogRead(pins_[i]);
+        //Serial.print(irVal[i]);
+        //Serial.print(" index:");
+        //Serial.println(i);
+        //Serial.print(irVal[i]);
+        irVal[i] = constrain(irVal[i], calib_from_vals[2*i], calib_from_vals[2*i+1]);
+        irVal[i] = map(irVal[i], calib_from_vals[2*i], calib_from_vals[2*i+1], CALIB_TO_LOW, CALIB_TO_HIGH);
+    }
+    return irVal;
+}
 
-       return num/den;
+void irArray::calibrate() {
+
+    //Serial.print("hello\n");
+    
+    //Serial.print("hi\n");
+    int startTime = millis();
+    int currentTime = millis();
+
+    int initVal = analogRead(pins_[0]);
+    
+    //intializing values in the array
+    for(int i = 0; i < numSensors*2; i++) {
+        calib_from_vals[i] = initVal;
     }
 
-    float irArray::read() {
-        float irVal[numSensors];
-        
-        for(int i = 0; i < numSensors-1; i++) {
-            irVal[i] = constrain(analogRead(pins_[i]), calib_from_vals[i], calib_from_vals[i+1]);
-            irVal[i] = map(irVal[i], calib_from_vals[i], calib_from_vals[i+1], CALIB_TO_LOW, CALIB_TO_HIGH);
-        }
-
-        return irVal;
-    }
-
-    void irArray::calibrate(float &calibratedVals) {
-        float calibratedVals[numSensors*2];
-
-        int startTime = millis();
-        int currentTime = millis();
-
-        int initVal = analogRead(AnalogInputPin0);
-        
-        //intializing values in the array
-        for(int i = 0; i < numSensors*2; i++) {
-            calibratedVals[i] = initVal;
-        }
-
-        while((currentTime - startTime) < CALIB_TIME) {
-            for(int i = 0; i < numSensors; i+=2) {
-                current_value = analogRead(pins_[i]);
-                if(current_value < calibratedVals[i]) {
-                    calibratedVals[i] = current_value;
-                } else if(current_value > calibratedVals[i+1]) {
-                    calibratedVals[i+1] = current_value;
-                }
+    while((currentTime - startTime) < CALIB_TIME) {
+        //Serial.print("hi\n");
+        for(int i = 0; i < numSensors; i++) {
+            float current_value = analogRead(pins_[i]);
+            if(current_value < calib_from_vals[2*i]) {
+                calib_from_vals[2*i] = current_value;
+            } else if(current_value > calib_from_vals[2*i+1]) {
+                calib_from_vals[2*i+1] = current_value;
             }
-
-            currentTime = millis();
         }
 
+        currentTime = millis();
     }
-};
+    //Serial.print("numSensors");
+}
+
+float* irArray::get_calib(){
+  return calib_from_vals;
+}
