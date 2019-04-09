570f0de00dfd3594a84b6791b111d5e404c07be7
nateriek <32182835+nateriek@users.noreply.github.com>
2019-04-09 19:33:03 -0400

    New Values

--- main/config.h
+++ main/config.h
@@ -4,29 +4,30 @@
 #include <Arduino.h>
 
 //IR pins
-#define IR1_PIN A0
-#define IR2_PIN A1
-#define IR3_PIN A2
-#define IR4_PIN A3
+#define IR1_PIN A2
+//#define IR2_PIN A1
+#define IR3_PIN A3
+//#define IR4_PIN A3
 #define IR5_PIN A4
-#define IR6_PIN A5
-#define IR7_PIN A6
-#define IR8_PIN A7
-#define IR9_PIN A8
-#define NUM_SENSORS 9
+//#define IR6_PIN A5
+#define IR7_PIN A5
+//#define IR8_PIN A7
+#define IR9_PIN A6
+#define NUM_SENSORS 5
+#define CALIB_ARRAY_SIZE 10
 
-#define PITCH 4 //distance between IR sensors in mm
+#define PITCH 8 //distance between IR sensors in mm
 #define CALIB_TO_LOW 1 //low value we want to map ir readings to
 #define CALIB_TO_HIGH 1000 //high value we want to map ir readings to
 #define CALIB_TIME 5000 //how long we want to calibrate for
 
 //Motor pins
-#define PWM_PIN_1 5
-#define PWM_PIN_2 11
-#define MOTOR_L_1 A9
-#define MOTOR_L_2 A10
-#define MOTOR_R_1 A11
-#define MOTOR_R_2 A12
+#define PWM_PIN_1 A0
+#define PWM_PIN_2 A1
+#define MOTOR_L_1 D4
+#define MOTOR_L_2 D5
+#define MOTOR_R_1 D2
+#define MOTOR_R_2 D3
 #define MAX_SPEED 255
 
 //Controller parameters
@@ -35,6 +36,6 @@
 #define K_D .01
 #define I_LWR_BND -40
 #define I_UPR_BND 40
-#define SETPOINT 500
+#define SETPOINT 0
 
 #endif
